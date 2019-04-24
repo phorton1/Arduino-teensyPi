f5e5815742f03e1908a1f381a6afa8ce3c32874c
Patrick Horton <available@upon.request>
2019-04-24 18:04:42 -0500

    added a couple more buttons and some Pots for looper

--- teensyPi.ino
+++ teensyPi.ino
@@ -3,6 +3,14 @@
 #include "rpiSerialMonitor.h"
 
 #define TEST_KEYPAD   0
+#define TEST_LOOPER_BUTTONS  1
+
+#if TEST_LOOPER_BUTTONS
+    #define PIN_BUTTON1    18
+    #define PIN_BUTTON2    19
+    #define PIN_VOLUME1    20
+    #define PIN_VOLUME2    21
+#endif
 
 
 #define PIN_ONBOARD_LED     13
@@ -42,6 +50,7 @@ void setup()
 { 
     Serial.begin(115200);   // 115200);   // 921600);
     Serial1.begin(115200);
+    pinMode(PIN_ONBOARD_LED,OUTPUT);
     
     #if TEST_KEYPAD
         for (int i=0; i<KP_NUM_ROWS*KP_NUM_COLS; i++)
@@ -54,6 +63,13 @@ void setup()
         }
     #endif
 
+    #if TEST_LOOPER_BUTTONS
+        pinMode(PIN_BUTTON1,INPUT_PULLUP);
+        pinMode(PIN_BUTTON2,INPUT_PULLUP);
+        pinMode(PIN_VOLUME1,INPUT_PULLDOWN);
+        pinMode(PIN_VOLUME2,INPUT_PULLDOWN);
+    #endif
+    
     delay(1200);
     display(0,"teensyPi started",0);
 }
@@ -66,6 +82,40 @@ void loop()
     
     #if TEST_KEYPAD
         keypad.task();
+    #endif
+
+    #if TEST_LOOPER_BUTTONS
+        #define VOLUME_DRIFT 2
+        static int volume1 = 0;
+        static int volume2 = 0;
+        static uint8_t button1_state = 0;
+        static uint8_t button2_state = 0;
+        static elapsedMillis vol_check = 0;
+        
+        if (myButtonPressed(PIN_BUTTON1,&button1_state))
+            display(0,"Loop Button 1 pressed",0);
+        if (myButtonPressed(PIN_BUTTON2,&button2_state))
+            display(0,"Loop Button 2 pressed",0);
+        
+        if (vol_check > 100)
+        {
+            vol_check = 0;
+            int vol1 = map(analogRead(PIN_VOLUME1),0,1023,0,101)-2;
+            if (vol1 < 0) vol1 = 0;
+            if (abs(vol1-volume1) > VOLUME_DRIFT)
+            {
+                volume1 = vol1;
+                display(0,"Volume1 == %d",volume1);
+            }
+            
+            int vol2 = map(analogRead(PIN_VOLUME2),0,1023,0,101)-2;
+            if (vol2 < 0) vol2 = 0;
+            if (abs(vol2-volume2) > VOLUME_DRIFT)
+            {
+                volume2 = vol2;
+                display(0,"Volume2 == %d",volume2);
+            }
+        }
     #endif
     
     if (blink_time > (blink_state ? BLINK_ON_TIME : BLINK_OFF_TIME))
