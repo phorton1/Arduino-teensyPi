19f188dec3d17da90d110e6ac4e5c45124ebb95f
Patrick Horton <available@upon.request>
2019-04-24 16:31:34 -0500

    added myKeyPad, cleaned up rpiMonitor

--- teensyPi.ino
+++ teensyPi.ino
@@ -1,50 +1,71 @@
 #include "myDebug.h"
+#include "myKeyPad.h"
 #include "rpiSerialMonitor.h"
 
-#define SENSE_RPI   1
+#define TEST_KEYPAD   0
 
-#if SENSE_RPI
-    #define PIN_RPI_SENSE      2      // handled by Looper.ino for teensy case
-    #define PIN_RPI_INDICATOR 11      // handled by Looper.ino for teensy case
-#endif
 
 #define PIN_ONBOARD_LED     13
-
-#define BLINK_ON_TIME   50
-#define BLINK_OFF_TIME 950
+#define BLINK_ON_TIME       50
+#define BLINK_OFF_TIME      950
 elapsedMillis blink_time = 0;
 int blink_state = 0;
 
 
 rpiSerialMonitor monitor(0,0);
 
+#if TEST_KEYPAD
+
+    myKeyPad keypad;
+    
+    void downHandler(void *param, int row, int col, uint8_t state)
+    {
+        display(0,"downHandler(%d,%d) param(%d) state(0x%02X)",row,col,(int)param,state);
+    }
+    void upHandler(void *param, int row, int col, uint8_t state)
+    {
+        display(0,"upHandler(%d,%d) param(%d) state(0x%02X)",row,col,(int)param,state);
+    }
+    void shortHandler(void *param, int row, int col, uint8_t state)
+    {
+        display(0,"shortHandler(%d,%d) param(%d) state(0x%02X)",row,col,(int)param,state);
+    }
+    void longHandler(void *param, int row, int col, uint8_t state)
+    {
+        display(0,"longHandler(%d,%d) param(%d) state(0x%02X)",row,col,(int)param,state);
+    }
+
+#endif  // TEST_KEYPAD
+
 
 void setup() 
 { 
     Serial.begin(115200);   // 115200);   // 921600);
     Serial1.begin(115200);
-    delay(1000);
-    display(0,"teensyPi started",0);
-    pinMode(PIN_ONBOARD_LED, OUTPUT);
-
-    #if SENSE_RPI
-        pinMode(PIN_RPI_SENSE,INPUT_PULLDOWN);
-        pinMode(PIN_RPI_INDICATOR,OUTPUT);
-        digitalWrite(PIN_RPI_INDICATOR,0);
+    
+    #if TEST_KEYPAD
+        for (int i=0; i<KP_NUM_ROWS*KP_NUM_COLS; i++)
+        {
+            keyPadButton *button = keypad.getButton(i);
+            button->registerHandler((void *)237,KP_STATE_DOWN,&downHandler);
+            button->registerHandler((void *)237,KP_STATE_UP,&upHandler);
+            button->registerHandler((void *)237,KP_STATE_SHORT,&shortHandler);
+            button->registerHandler((void *)237,KP_STATE_LONG,&longHandler);
+        }
     #endif
+
+    delay(1200);
+    display(0,"teensyPi started",0);
 }
 
 
 
 void loop() 
 {
-    #if SENSE_RPI
-        static uint8_t teensy_sensed = 0;
-        if (digitalRead(PIN_RPI_SENSE) != teensy_sensed)
-        {
-            teensy_sensed = !teensy_sensed;
-            digitalWrite(PIN_RPI_INDICATOR,teensy_sensed);
-        }
+    monitor.task();
+    
+    #if TEST_KEYPAD
+        keypad.task();
     #endif
     
     if (blink_time > (blink_state ? BLINK_ON_TIME : BLINK_OFF_TIME))
@@ -53,5 +74,4 @@ void loop()
         blink_state = !blink_state;
         digitalWrite(PIN_ONBOARD_LED,blink_state);
     }
-    monitor.task();
 }
