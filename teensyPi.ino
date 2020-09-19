3f6e3eba0bf8d4d292ecb09534cf5de4759c7afa
phorton1 <available@upon.request>
2020-09-19 18:16:39 -0500

    FIRST INTEGRATED LOOPER AND TEENSY-EXPRESSION TEST Here on the teensyExpression and teensyPi side teensyPi now recognizes my "midi packets" coming in over the Serial2    port from the teensyExpression, and forwards them to the rPi.    It can also echo the teensExpression debugging output at the    same time (for debugging when TE is attached to iPad). teensyExpression implements rotary controllers for the rPi Looper    INPUT, OUTPUT, THRU, and MIX controls via the serial midi protocol,    and the rPi Looper LOOP volume is controlled over the serial midi    protocol from the TE "loop expression pedal", via a somewhat    generalized new implementation of "PEDAL MODES".

--- teensyPi.ino
+++ teensyPi.ino
@@ -1,9 +1,6 @@
 #include "myDebug.h"
 #include "rpiSerialMonitor.h"
 
-#define WITH_SERIAL2   1
-
-
 #define PIN_ONBOARD_LED     13
 #define BLINK_ON_TIME       50
 #define BLINK_OFF_TIME      950
@@ -20,17 +17,12 @@ void setup()
     display(0,"teensyPi v1.4 started",0);
 
     Serial1.begin(115200);
+    display(0,"opening Serial2",0);
+    Serial2.begin(115200);
 
     pinMode(PIN_ONBOARD_LED,OUTPUT);
 
-
-    #if WITH_SERIAL2
-        display(0,"opening Serial2",0);
-        Serial2.begin(115200);
-    #endif
-
     monitor.init();
-
     monitor.rebootPi();
         // if both are started at the same time,
         // the teensy supresses the pi bootstrap
@@ -42,17 +34,81 @@ void loop()
 {
     monitor.task();
 
-    #if WITH_SERIAL2
-        // If anything on the coming over serial 2
-        // write it to the PI AND as a message over the main Serial
-        if (Serial2.available())
-        {
-            int c = Serial2.read();
-            Serial1.write(c);
-            display(0,"serial2 received chr(%d) and sent it to Serial1",c);
-        }
-    #endif
+    // My midi protocol
+    //
+    // is 4 byte packets that look just like USB midi packets
+    //
+    //     CIN           |  MIDI0   |  MIDI1   |  MIDI2   |
+    //    byte0          |  byte1   |  byte2   |  byte3   |
+    //-------------------------------------------------------
+    // cable  |  code    |          |          |          |
+    // number |  index   |          |          |          |
+    //        |  number  |          |          |          |
+    //
+    // The "cable number" is always zero.
+    //
+    // The CIN may be one of the standard values from 0x00 to 0x0F,
+    // EXCEPT that 0xA and 0xD, which are seldom used MIDI packets
+    // for "poly-keypress" and "channel pressure" ARE NOT USED.
+    // Those values are "reserved" for TEXT carriage returns and
+    // linefeeds, so that this protocol can be sent "within" a
+    // potential text streem (i.e. debugging output).
+    //
+    // Thus, when we see a byte 0x0 thru 0xF (except 0xA and 0xD)
+    // we presume there is a 4 byte (32 bit) midi packet present,
+    // which we pass onto the raspberry pi via the main serial
+    // port.
+    //
+    // The protocol is bi-directional.
 
+    // So, if we identify a packet, we pass it on to the rPI.
+    // otherwise, we echo the output to the teensyPi USB port
+    // and see what happens.  To make debugging output look right,
+    // we have to buffer, and break on carriage returns,
+    // and present a "header" before the line oriented degug output
+
+    #define MAX_LINE_BUFFER 512
+    static int midi_ptr = 0;
+    static int line_ptr = 0;
+    static unsigned char midi_buffer[4];
+    static unsigned char line_buffer[MAX_LINE_BUFFER];
+
+    if (Serial2.available())
+    {
+        unsigned char c = Serial2.read();
+        // warning(0,"teensyPI got SERIAL2(0x%02x == %d) '%c'",c,c,(c>32?c:32));
+
+        if (midi_ptr)
+        {
+            midi_buffer[midi_ptr++] = c;
+            // display(0,"teensyPI adding to midi packet(0x%02x == %d)",c,c);
+            if (midi_ptr == 4)
+            {
+                // display_bytes(0,"teensyPi sending packet to rPI",midi_buffer,4);
+                Serial1.write(midi_buffer,4);
+                midi_ptr = 0;
+            }
+        }
+        else if (c <= 0xf && c != 0xA && c != 0xD)
+        {
+            midi_buffer[midi_ptr++] = c;
+            // display(0,"teensyPI starting midi packet with (0x%02x == %d)",c,c);
+        }
+        else if (c == 0xA || c == 0xD || line_ptr >= MAX_LINE_BUFFER-3)
+        {
+            if (line_ptr)
+            {
+                display(0,"TE: %s",line_buffer);
+            }
+            line_buffer[0] = 0;
+            line_ptr = 0;
+        }
+        else
+        {
+            line_buffer[line_ptr++] = c;
+            line_buffer[line_ptr] = 0;
+        }
+    }
 
     if (blink_time > (blink_state ? BLINK_ON_TIME : BLINK_OFF_TIME))
     {
